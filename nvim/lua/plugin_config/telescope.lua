fa65d33185b20c4fa06b67af22220af8f114c7a2
Semir Ramovic <semir.ramovic@easelink.com>
2024-02-06 12:16:15 +0100

    telescope binding to show buffers. python3 provider global.

--- nvim/lua/plugin_config/telescope.lua
+++ nvim/lua/plugin_config/telescope.lua
@@ -5,7 +5,8 @@ vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<c-p>', builtin.find_files, {})
 vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, {})
+vim.keymap.set('n', '<c-p>', builtin.find_files, {})
 vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<Space>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<Space>fb', builtin.buffers, {})
+-- vim.keymap.set('n', '<Space>fh', builtin.help_tags, {})
