66f4b9abf9a3df0e4e6238b5b8d40ef015d97518
Semir Ramovic <semir.ramovic@easelink.com>
2024-02-08 09:07:12 +0100

    add fish

---
+++ fish/config.fish
@@ -0,0 +1,48 @@
+abbr -a c cargo
+abbr -a e nvim
+abbr -a g git
+abbr -a ga 'git add .'
+abbr -a gl 'git log'
+abbr -a gd 'git diff'
+abbr -a gs 'git status'
+abbr -a gpl 'git pull'
+abbr -a gph 'git push; and git push --tags'
+abbr -a gah 'git stash; and git pull --rebase; and git stash pop'
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_prompt
+	set_color brblack
+	echo -n "["(date "+%H:%M")"] "
+	set_color blue
+	echo -n (hostnamectl hostname)
+	if [ $PWD != $HOME ]
+		set_color brblack
+		echo -n ':'
+		set_color yellow
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (__fish_git_prompt)
+	set_color red
+	echo -n '| '
+	set_color normal
+end
+
+function fish_greeting
+	echo
+	echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e " \\e[1mDisk usage:\\e[0m"
+	echo
+	echo -ne (\
+		df -l -h | grep -E 'dev/(xvda|sd|mapper)' | \
+		awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
+		sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
+		paste -sd ''\
+	)
+	echo
+end
