ac78ced43a96d508f4c7229bd22431ca2bd5f06b
Semir Ramovic <semir.ramovic@outlook.com>
2025-08-18 23:09:23 +0200

    add modified nvim kickstart

---
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,30 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = false, -- Show filtered items (dotfiles, etc.)
+        hide_dotfiles = true, -- Don’t hide files like `.env`
+        hide_gitignored = true, -- Optional: Show files in .gitignore
+      },
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
--- nvim/lua/plugin_config/colorscheme.lua
+++
@@ -1,5 +0,0 @@
-require("gruvbox").setup({})
-
-vim.o.termguicolors = true
-vim.o.background = "dark"
-vim.cmd[[colorscheme gruvbox]]
